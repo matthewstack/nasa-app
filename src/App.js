1c18aa912754240f7b436b18407782d006ef2e32
Matthew Stack <matthewstack@Matthews-MacBook-Pro-4.local>
2021-08-12 22:11:34 -0400

    Uploading project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import axios from 'axios'
+import { BASE_URL, API_KEY } from './constants/index'
+import { Container, Card, Carousel, Row, Col, ListGroup, Navbar, Button } from 'react-bootstrap'
+
+
 
 function App() {
+  const [photos, setPhotos] = useState([])
+
+  
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}&start_date=2021-07-28`)
+      .then(res => {
+        setPhotos(res.data);
+        setImg(res.data[0])
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [])
+
+  const [featuredImg, setImg] = useState(photos[0])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Container fluid>
+            <Navbar className="justify-content-center" bg="primary" variant="dark">
+              <Navbar.Brand href="#home">
+                <h1>NASA PICTURE OF THE DAY</h1>
+              </Navbar.Brand>
+            </Navbar>
+          </Container>
+
+        <Container fluid>
+            <Row>
+                  <Col sm={3}>
+                    <ListGroup defaultActiveKey="#link1">
+                      <ListGroup.Item variant="primary">CLICK BELOW TITLES TO SET FEATURED IMAGE</ListGroup.Item>
+                      { photos.map ((photo, index) => {
+                        return <ListGroup.Item action onClick={ () =>setImg(photos[index])}>{photo.title}</ListGroup.Item>
+                      })}
+
+                    </ListGroup>          
+                  </Col>
+
+                  <Col sm={9}>
+                    <Card>
+                      <Card.Img variant="top" src={featuredImg.url}/>
+                      <Card.Body>
+                        <Card.Title>{featuredImg.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{featuredImg.date}</Card.Subtitle>
+                        <Card.Text>{featuredImg.explanation}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+            </Row>
+        </Container>
     </div>
   );
 }
